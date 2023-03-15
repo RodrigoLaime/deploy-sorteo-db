b5e667743e5301035e94378887f752e10af45f7a
rodrigo laime <rodrigolaime141@gmail.com>
2023-03-15 17:07:44 -0300

    styles good

--- src/public/js/getSorteo.js
+++ src/public/js/getSorteo.js
@@ -3,19 +3,37 @@ async function getSorteo() {
   const response = await fetch("/api/winner");
   const data = await response.json();
   console.log(data);
-
+  let num = 0;
   data.data.forEach(user => {
-    const usuario = `<li>name: ${user.name}</li>`;
-
+    num += 1
+    /*     const usuario = `<li>name: ${user.name}</li>`; */
     const winnerUl = document.getElementById('winnerUl');
     if (user.lengt === 0) {
       winnerUl.innerHTML = 'No hay ganadores para mostrar'
     } else {
-      winnerUl.insertAdjacentHTML('beforeend', usuario)
+      /*    winnerUl.insertAdjacentHTML('beforeend', usuario) */
+      mostrarWinner(user, num)
     }
   });
 
-
 }
+/*   const table = document.getElementById('table'); */
+/*   const head = document.getElementById('thead'); */
+/*   head.insertAdjacentHTML('afterend', body) */
+const mostrarWinner = (user, numero) => {
+  const body = document.getElementById('tbody');
+  //crear element
+  /*   const tr1 = body.insertRow('tr'); */
+  const tr1 = document.createElement('tr');
+  const td1 = tr1.insertCell('td');
+  const td2 = tr1.insertCell('td');
+  const td3 = tr1.insertCell('td');
+  td1.innerText = `${user.amount}`
+  td2.innerText = `${user.name}`
+  td3.innerText = numero
+  body.appendChild(tr1);
+}
+/* mostrarWinner(); */
+
 
 getSorteo()
--- src/public/js/sorteo.js
+++ src/public/js/sorteo.js
@@ -95,7 +95,7 @@ function sorteoDos(input, agregar, ganador, jugadores) { //agregamos
       modalFunction()
 
     };//----------
-
+    /*   getProduct(UserGandor);// */
 
     setTimeout(() => {
       $jugadores.innerHTML = "";
@@ -126,8 +126,8 @@ function sorteoDos(input, agregar, ganador, jugadores) { //agregamos
 };
 
 /* ########################################## */
-/* const apiPatch = 'https://deploy-sorteo-db-production-d3f8.up.railway.app/api/update/' */
-const apiPatch = 'http://localhost:3000/api/update/'
+const apiPatch = 'https://deploy-sorteo-db-production-d3f8.up.railway.app/api/update/'
+/* const apiPatch = 'http://localhost:3000/api/update/' */
 async function updateData(id, nombre, amount) {
   const response = await fetch(apiPatch + id, {
     method: 'PATCH',
@@ -147,8 +147,8 @@ async function updateData(id, nombre, amount) {
 /* ########################################## */
 
 //funcion para agregar a la base de datos
-const api = 'http://localhost:3000/api/sorteo'
-/* const api = 'https://deploy-sorteo-db-production-d3f8.up.railway.app/api/sorteo' */
+/* const api = 'http://localhost:3000/api/sorteo' */
+const api = 'https://deploy-sorteo-db-production-d3f8.up.railway.app/api/sorteo'
 async function postData(name, repeat) {
   const response = await fetch(api, {
     method: 'POST',
